4301a19960ad45064f5b1bd5569fec975664621f
leesaw <kmitn@kmitns-MacBook-Air.local>
2015-05-25 14:11:08 +0700

    change config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://127.0.0.1/cien';
+$config['base_url']	= 'http://127.0.0.1/cien_git';
 
 /*
 |--------------------------------------------------------------------------
